11617cad34399da2034176c9ee6349535d4e858d
anuj <anuj@gmail.com>
2024-11-25 13:33:09 +0530

    condition

---
+++ oddeven.py
@@ -0,0 +1,17 @@
+number= int (input("enter any number"))
+if number>0:
+   print("your no is +ve")
+   if number %2==00:
+     print("your number is even")
+   else:
+     print("your number is odd")
+
+
+else:
+  print("your no is -ve")
+  if number %2==00:
+     print("your number is even")
+  else:
+     print("your number is odd")
+
+  
