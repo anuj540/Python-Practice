88c3fde00865ac32a627b722800d387883b238af
anuj <anuj@gmail.com>
2024-11-26 13:04:26 +0530

    for loops

---
+++ loops.py
@@ -0,0 +1,26 @@
+
+# for a in range(3,21):
+#     if a %2==0:
+#         print(a)
+    
+#table 2
+
+# for a in range(2,21):
+#     if a%2==0:
+#         print(a)
+
+#table 3
+
+# for a in range(3,31):
+#     if a %3==0:
+#         print(a)
+
+        
+#table 4
+
+# for a in range (4,41):
+#     if a%4==0:
+#         print(a)
+
+# for a in range (3,20,4):
+#     print(a)
