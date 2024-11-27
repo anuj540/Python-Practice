c962e702f37823917505cc0b366e6ebc0105f7ad
anuj <anuj@gmail.com>
2024-11-28 00:11:51 +0530

    loops

--- while.py
+++ while.py
@@ -8,12 +8,19 @@
 # print(sum)
     
 #print prime number
-prime =1
-if prime > 1:
- for a in range(13 ):
-    if(prime%1)  ==1:
+# prime =1
+# if prime > 1:
+#  for a in range(13 ):
+#     if(prime%1)  ==1:
 
-     print(prime)
+#      print(prime)
     
 
     
+a=1
+while a<=20:
+    if(a%2==0):
+     a=a+1
+     continue
+    print(a)
+    a=a+1
