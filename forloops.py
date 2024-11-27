e058f58f6b96c52b5528b6090fd0355d2882f040
anuj <anuj@gmail.com>
2024-11-27 22:43:03 +0530

    loops

---
+++ forloops.py
@@ -0,0 +1,14 @@
+# for a in range(0,20):
+     
+#     if a==13:
+#         break
+#     print(a)
+# a= int(input("enter number"))
+# for a in range(a):
+#      if  a==12 or a==17 : 
+#           continue
+#      print(a)
+
+
+
+
---
+++ while.py
@@ -0,0 +1,19 @@
+# a=2
+# while a<=20:
+#  print(a)
+#  a=a+2
+# sum=0
+# for a in range(10):
+#     sum=sum+a
+# print(sum)
+    
+#print prime number
+prime =1
+if prime > 1:
+ for a in range(13 ):
+    if(prime%1)  ==1:
+
+     print(prime)
+    
+
+    
